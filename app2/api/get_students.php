34bf0ba8c25820e7524c4dcc936777c75bfdf156
Loki <summer10920@gmail.com>
2020-06-18 08:56:40 +0800

    新增修改功能

--- app2/api/get_students.php
+++ app2/api/get_students.php
@@ -3,9 +3,12 @@ include "base.php";
 
 $class=$_GET['class'];
 $db=new DB("students");
-$students=$db->q("select name,class_num,dept,nat_id from students where substring(`class_num`,1,3)='$class'");
+$students=$db->q("select id,name,class_num,dept,nat_id from students where substring(`class_num`,1,3)='$class'");
 foreach($students as $stu){
-    echo "<div class='stu'>".$stu['name']."</div>";
+    echo "<div class='stu'>".$stu['name'];
+    echo   "<button onclick='update(".$stu['id'].")'>編輯</button>";
+    echo   "<button onclick='del(".$stu['id'].")'>刪除</button>";
+    echo "</div>";
 }
 
 
