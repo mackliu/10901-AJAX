7b98e1470530bc875681f683ee8808f8ce46e8f9
mack <macklun@gmail.com>
2020-06-18 09:33:27 +0800

    新增編輯用的按鈕及表單載入

--- app2/api/get_students.php
+++ app2/api/get_students.php
@@ -7,7 +7,7 @@ $db=new DB("students");
 $students=$db->q("select id,name,class_num,dept,nat_id from students where substring(`class_num`,1,3)='$class'");
 foreach($students as $stu){
     echo "<div class='stu'>".$stu['name'];
-    echo   "<button onclick='update(".$stu['id'].")'>編輯</button>";
+    echo   "<button onclick='getUpdateForm(".$stu['id'].")'>編輯</button>";
     echo   "<button onclick='del(".$stu['id'].")'>刪除</button>";
     echo "</div>";
 }
